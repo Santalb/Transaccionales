abe7fa17e5931ff3168c9361a86b6ce92a95b0cb
Santalb <143513988+Santalb@users.noreply.github.com>
2023-08-29 08:24:27 -0500

    Add files via upload

---
+++ proyecto-spring/src/main/java/pe/net/proyectolg/proyectotransac/ProyectoTransacApplication.java
@@ -0,0 +1,13 @@
+package pe.net.proyectolg.proyectotransac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTransacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTransacApplication.class, args);
+	}
+
+}
