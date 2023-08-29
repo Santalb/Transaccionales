44069f76f0fd1217cdd3f40987b0f8b300c47d30
Santalb <143513988+Santalb@users.noreply.github.com>
2023-08-29 08:11:36 -0500

    Add files via upload

---
+++ src/test/java/pe/net/proyectolg/proyectotransac/ProyectoTransacApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.net.proyectolg.proyectotransac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTransacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
